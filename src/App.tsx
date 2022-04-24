1b3b4824a8e3be0fb78d3fa0c18e09f1ef28ccb8
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-04-24 06:36:30 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from "components/routes/Home";
-import Contact from "components/routes/Contact";
 import Layout from "components/layouts";
 
 import { routes } from "utils/enum";
@@ -10,10 +9,7 @@ import { routes } from "utils/enum";
 const App = () => {
   return (
       <Routes>
-            <Route path={routes.HOME} element={<Layout> <Home /> </Layout>} /> 
-
-             <Route path={routes.CONTACT} element={<Layout> <Contact /> </Layout>} /> 
-    
+            <Route path={routes.HOME} element={<Layout> <Home /> </Layout>} />     
       </Routes>
   )
 }
