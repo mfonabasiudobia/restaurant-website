ea0447d18a1dc8c390e15e5df33ab7a83da86d6e
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-04-24 06:55:01 +0100

    Update

--- src/components/partials/cards/Product.tsx
+++ src/components/partials/cards/Product.tsx
@@ -3,11 +3,11 @@ import { AiFillEye, AiOutlineLink } from "react-icons/ai";
 import { products } from "../../../data/products";
 
 const styles = {
-	title : `text-6xl font-bold text-yellow-500`,
-	subTitle : `text-4xl font-bold`,
+	title : `text3xl md:text-6xl font-bold text-yellow-500`,
+	subTitle : `text-xl md:text-4xl font-bold`,
 	container : `container space-y-7`,
 	header : `space-y-2 text-center`,
-	body : `grid grid-cols-2 md:grid-cols-3 gap-10`,
+	body : `grid md:grid-cols-3 gap-10`,
 	item : `relative group space-y-4 cursor-pointer`,
 	itemImage : `w-full  h-full object-cover group-hover:scale-150`,
 	imageWrapper : `h-[30vh] overflow-hidden relative`,
